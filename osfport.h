e33afaebc3ceb8ce8825172e08a3eec8ebe8d5c2
JayKrell <jay.krell@cornell.edu>
2021-07-05 01:25:18 -0700

    stuff

--- osfport.h
+++ osfport.h
@@ -24,11 +24,8 @@
  */
 #if defined(__osf__) && defined(__alpha__) && defined(__arch64__)
 
-#if _ISO_C_SOURCE < 19990
-#error please define _ISO_C_SOURCE >= 19990 on command line, in case this file is not included early enough
-#endif
-
-/* This file should be included relatively early in order to get the defines "correct". */
+#undef _ISO_C_SOURCE
+#define _ISO_C_SOURCE 19990
 #define _OSF_SOURCE
 #undef _XOPEN_SOURCE_EXTENDED
 #include <grp.h>
@@ -67,83 +64,30 @@ copysign prototype is in math.h, disabled by _XOPEN_SOURCE_EXTENDED, yes, disabl
  * should get its prototype, at least with cc, not gcc
  * see /usr/lib/cmplrs/cc.dtk/EIDF.h.
  */
+double round(double);
 
-/* C99 SIZE_MAX is missing, try to use gcc's predefine. */
-#ifndef SIZE_MAX
-#ifdef __SIZE_MAX__
-#define SIZE_MAX __SIZE_MAX__
-#else
-/* ULLONG_MAX ought to work, but it is only in sm/limits.h (sendmail) */
-#define SIZE_MAX ULONG_MAX
-#endif
-#endif
-
-/* ditto INTMAX_MAX */
-#ifndef INTMAX_MAX
-#ifdef __INTMAX_MAX__
-#define INTMAX_MAX __INTMAX_MAX__
-#else
-/* ULLONG_MAX ought to work, but it is only in sm/limits.h (sendmail) */
-#define INTMAX_MAX LONG_MAX
-#endif
-#endif
-
-/* C99 types might be missing; long or long long are ok. Try gcc's predefines. */
-/* Sometimes they are defines. */
-#undef intmax_t
-#ifdef __INTMAX_TYPE__
-typedef __INTMAX_TYPE__ intmax_t;
-#else
+#define SIZE_MAX 18446744073709551615UL
+#define INTMAX_MAX 9223372036854775807L
+#define UINTMAX_MAX 18446744073709551615UL
 typedef long intmax_t;
-#endif
-
-#undef uintmax_t
-#ifdef __UINTMAX_TYPE__
-typedef __UINTMAX_TYPE__ uintmax_t;
-#else
 typedef unsigned long uintmax_t;
-#endif
 
 /* C99 printf macros are missing. */
-/* l or ll are ok, _LONGLONG is defined, indicating long long exists */
 #define PRId64 "ld"
 #define PRIi64 "li"
 #define PRIo64 "lo"
 #define PRIu64 "lu"
 #define PRIx64 "lx"
-
 #define PRIdMAX "ld"
 #define PRIiMAX "li"
 #define PRIoMAX "lo"
 #define PRIuMAX "lu"
 #define PRIxMAX "lx"
 
-/* C99 integer constant macros are missing
- * When using gcc use its predefines.
- */
-#ifndef UINT16_C
-#ifdef __UINT16_C
-#define UINT16_C(c) __UINT16_C(c)
-#else
+/* C99 integer constant macros are missing */
 #define UINT16_C(c) c
-#endif
-#endif
-
-#ifndef UINT32_C
-#ifdef __UINT32_C
-#define UINT32_C(c) __UINT32_C(c)
-#else
 #define UINT32_C(c) c ## U
-#endif
-#endif
-
-#ifndef UINT64_C
-#ifdef __UINT64_C
-#define UINT64_C(c) __UINT64_C(c)
-#else
 #define UINT64_C(c) c ## UL
-#endif
-#endif
 
 /* This version may not work in an .so. Another version is in newer gnulib.
 * Place this in getprogname.c as needed. It occurs in many programs.
