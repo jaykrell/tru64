af2e9c8782320f9208d7b75c7c3ab2977e2d9d04
JayKrell <jay.krell@cornell.edu>
2021-07-06 22:39:03 -0700

    better osfport.h

--- osfport.h
+++ osfport.h
@@ -1,37 +1,23 @@
 /* OSF/1 is missing a few things.
  * uname -srm: OSF1 V5.1 alpha
  *
- * C99 support is incomplete.
- * There is long long, but long is also 64bits so long long is not really neded.
- * Prototypes are behind ifdef *and* sometimes ifndef.
- * It is not obvious if one set of defines
- * will reveal enough, since some defines
- * actually hide prototypes, but let's try.
- *
- * This one file has all the workarounds and
- * is included whenever any of them are needed.
- *
- * Better fixes would involve autoconf
- * and include, however hardcoding here
- * is less disruptive and the system is unlikely
- * to change, and older versions are not very interesting.?
- *
- * And again, some prototypes are actually behind ifndef.
- *
- * Most of these workarounds are for Python3 but some apply to other programs.
- *
- * gcc also defines _LP64 but cc does not.
+ * This is mainly for Python3, so far.
  */
 #if defined(__osf__) && defined(__arch64__)
 
+/* various standard facilities require specific request to reveal */
 #define __STDC_CONSTANT_MACROS 1
 #define __STDC_LIMIT_MACROS 1
+#define __STDC_FORMAT_MACROS 1
+
 #undef _ISO_C_SOURCE
 #define _ISO_C_SOURCE 199901
 #define _OSF_SOURCE 1
 #define _ANSI_C_SOURCE 1
-#define _POSIX_C_SOURCE 199506L
-#undef _XOPEN_SOURCE_EXTENDED
+#define _XOPEN_SOURCE 700       /* The highest value in OSF1 is 500 but match Python3 */
+#define _POSIX_C_SOURCE 200809L /* The highest value in OSF1 is 199506L but match Python3 */
+#undef _XOPEN_SOURCE_EXTENDED   /* defining this, hides symbols such as NSIG, copysign */
+
 #include <sys/types.h>
 #include <grp.h>
 #include <signal.h>
@@ -39,18 +25,25 @@
 #include <wait.h>
 #include <limits.h>
 
+#include "/usr/include.dtk/stdint.h" /* for gcc */
+#include "/usr/include.dtk/inttypes.h" /* for gcc */
+
+#ifdef __SIZE_MAX__
+#define SIZE_MAX __SIZE_MAX__
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /*
-wait4 is in /usr/include/wait.h behind defined(_OSF_SOURCE) || defined(_XOPEN_SOURCE_EXTENDED)
-setegid is in /usr/include/unistd.h behind ifdef _OSF_SOURCE
-seteuid ditto
-setegid ditto
+wait4      is in /usr/include/wait.h behind defined(_OSF_SOURCE) || defined(_XOPEN_SOURCE_EXTENDED)
+setegid    is in /usr/include/unistd.h behind ifdef _OSF_SOURCE
+seteuid    ditto
+setegid    ditto
 initgroups is in /usr/include/grp.h unconditionally
-NSIG is behind ifndef _XOPEN_SOURCE_EXTENDED in /usr/include/signal.h
-copysign prototype is in math.h, disabled by _XOPEN_SOURCE_EXTENDED, yes, disabled
+NSIG       is behind ifndef _XOPEN_SOURCE_EXTENDED in /usr/include/signal.h, yes, ifndef
+copysign   is in math.h, disabled by _XOPEN_SOURCE_EXTENDED, yes, disabled
 *
 * If this does not work out, then try declaring things ourselves:
 *union wait;
@@ -70,29 +63,6 @@ copysign prototype is in math.h, disabled by _XOPEN_SOURCE_EXTENDED, yes, disabl
  * see /usr/lib/cmplrs/cc.dtk/EIDF.h.
  */
 double round(double);
-
-#define SIZE_MAX 18446744073709551615UL
-#define INTMAX_MAX 9223372036854775807L
-#define UINTMAX_MAX 18446744073709551615UL
-typedef long intmax_t;
-typedef unsigned long uintmax_t;
-
-/* C99 printf macros are missing. */
-#define PRId64 "ld"
-#define PRIi64 "li"
-#define PRIo64 "lo"
-#define PRIu64 "lu"
-#define PRIx64 "lx"
-#define PRIdMAX "ld"
-#define PRIiMAX "li"
-#define PRIoMAX "lo"
-#define PRIuMAX "lu"
-#define PRIxMAX "lx"
-
-/* C99 integer constant macros are missing */
-#define UINT16_C(c) c
-#define UINT32_C(c) c ## U
-#define UINT64_C(c) c ## UL
 
 /* This version may not work in an .so. Another version is in newer gnulib.
 * Place this in getprogname.c as needed. It occurs in many programs.
