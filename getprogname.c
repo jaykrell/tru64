c613646da9d6751b3e0287a8d51d8d0ae31b1469
JayKrell <jay.krell@cornell.edu>
2021-07-10 01:28:24 -0700

    getprogname

---
+++ getprogname.c
@@ -0,0 +1,10 @@
+/* Place this in getprogname.c as needed. It occurs in many programs. */
+
+extern char** __Argv;
+
+char const* last_component (const char*);
+
+char const* getprogname (void)
+{
+    return last_component (__Argv [0]);
+}
